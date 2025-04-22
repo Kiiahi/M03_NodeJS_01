0f57ba289a6c50ffeaadab1818874111ec0aeeef
Jiyah <jiyahcoleman00@gmail.com>
2025-04-22 11:17:36 -0500

    main.js
     update

---
+++ main.js
@@ -0,0 +1,14 @@
+      // Operations: Object Access, Invocation
+      console.log("What's your name?");
+
+      // Operations:Handles users input data
+      process.stdin.on('data', function(data) {
+
+          // Operations:converts input data to a string , trims whitespace , and assigns it to name
+          const name = data.toString().trim();
+          
+          // Operations:outputs greeting with the name to the console
+          console.log(`Hello, ${name}!`);
+
+      });
+      ```
